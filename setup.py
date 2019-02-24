14e624b7308ab22fa4cbc488ff12acc717646c58
Baris Demiray <baris.demiray@gmail.com>
2019-02-24 11:58:41 +0100

    T417 Update version to 0.0.9

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="statuscope",
-    version="0.0.8",
+    version="0.0.9",
     author="Baris Demiray",
     author_email="baris.demiray@gmail.com",
     description="A package to ease log sending to Statuscope",
